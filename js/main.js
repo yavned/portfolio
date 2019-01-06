ee54a5f466a4a43aa859e663fbb33689c37187b3
Yaroslav <n.123@i.ua>
2019-01-06 22:33:05 +0200

    v1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  menuBtn.classList.toggle('close');
+  menu.classList.toggle('show');
+  menuNav.classList.toggle('show');
+  menuBranding.classList.toggle('show');
+  navItems.forEach(item => item.classList.toggle('show'));
+}
